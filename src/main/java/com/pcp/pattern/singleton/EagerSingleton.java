80d737a3f112a62b6bb8f9f3e13ae5549428e83b
prabhupurohit <prabhu.purohit@gmail.com>
2023-05-30 17:23:52 +0530

    Initial Commit

---
+++ src/main/java/com/pcp/pattern/singleton/EagerSingleton.java
@@ -0,0 +1,31 @@
+package com.pcp.pattern.singleton;
+
+public class EagerSingleton {
+
+    //This is an information the Singleton class will hold and will be used
+    //by other classes
+    private String data = null;
+
+    //Eagerly create the instance of the Singleton class at class loading time
+    private static EagerSingleton instance = new EagerSingleton();
+
+    private EagerSingleton() {
+
+    }
+
+    public static EagerSingleton getInstance() {
+        if (instance != null) {
+            return instance;
+        } else {
+            return new EagerSingleton();
+        }
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
