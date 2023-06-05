2436f306e2dec8a3e2e192f6467d8365df47cb15
prabhupurohit <prabhu.purohit@gmail.com>
2023-06-05 22:02:39 +0530

    Adding Functional Interface technique for command pattern

--- src/main/java/com/pattern/behavioral/strategy/Strategy.java
+++ src/main/java/com/pattern/behavioral/strategy/Strategy.java
@@ -1,5 +1,7 @@
 package com.pattern.behavioral.strategy;
 
+//If there is one interface method, we can very well name it as a functional interface
+@FunctionalInterface
 public interface Strategy {
 
     public int doOperation(int num1, int num2);
--- src/main/java/com/pattern/behavioral/strategy/StrategyDemo.java
+++ src/main/java/com/pattern/behavioral/strategy/StrategyDemo.java
@@ -7,14 +7,23 @@ public class StrategyDemo {
         //Create a MathOperation Context
 
         MathOperationContext mathAddContext = new MathOperationContext(new AddOperation());
-
         System.out.println((mathAddContext.doMathOperation(15, 20)));
+
+        //Using Functional Interface and Lambda Expression
+        MathOperationContext mathAddContext1 = new MathOperationContext((a, b) -> a + b);
+        System.out.println((mathAddContext1.doMathOperation(15, 20)));
 
         MathOperationContext mathSubContext = new MathOperationContext(new SubstractOperation());
         System.out.println(mathSubContext.doMathOperation(50, 20));
 
+        MathOperationContext mathSubContext1 = new MathOperationContext((a, b) -> a - b);
+        System.out.println(mathSubContext1.doMathOperation(50, 20));
+
         MathOperationContext mathMulContext = new MathOperationContext(new MultiplyOperation());
         System.out.println(mathMulContext.doMathOperation(30, 10));
+
+        MathOperationContext mathMulContext1 = new MathOperationContext((a, b) -> a * b);
+        System.out.println(mathMulContext1.doMathOperation(30, 10));
 
     }
 }
